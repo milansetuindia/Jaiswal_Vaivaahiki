d7a057c4472acdf658f23cef63ef2df7ea035252
milansetuindia <milansetuindia@gmail.com>
2026-08-10 11:31:10 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// js/config.js
+
+window.APP_NAME = "Jaiswal Vaivaahiki";
+
+window.API_URL =
+window.location.hostname === "localhost"
+  ? "http://localhost:5000"
+  : "https://backend-milan-setu.onrender.com";
+
+window.RAZORPAY_KEY = "rzp_test_TNxSxzsIjGgsrZ";
---
+++ js/positions.js
@@ -0,0 +1,107 @@
+const POSITIONS = {
+
+    /*==============================
+            PERSONAL DETAILS
+    ==============================*/
+
+    previewName:           { left: 340, top: 100, width: 300, fontSize: 40, fontWeight: 900 },
+    previewDOB:            { left: 250, top: 280, width: 300, fontSize: 18, fontWeight: 600 },
+    previewTime:           { left: 250, top: 315, width: 300, fontSize: 18, fontWeight: 600 },
+    previewPlace:          { left: 250, top: 349, width: 300, fontSize: 18, fontWeight: 600 },
+
+    previewCaste:          { left: 250, top: 452, width: 300, fontSize: 18, fontWeight: 600 },
+
+    previewRashi:          { left: 250, top: 520, width: 300, fontSize: 18, fontWeight: 600 },
+    previewGan:            { left: 250, top: 555, width: 300, fontSize: 18, fontWeight: 600 },
+    previewManglik:        { left: 250, top: 589, width: 300, fontSize: 18, fontWeight: 600 },
+
+    previewMaritalStatus:  { left: 250, top: 623, width: 300, fontSize: 18, fontWeight: 600 },
+    previewDiet:           { left: 250, top: 657, width: 300, fontSize: 18, fontWeight: 600 },
+    previewHeight:         { left: 250, top: 691, width: 300, fontSize: 18, fontWeight: 600 },
+    previewComplexion:     { left: 250, top: 725, width: 300, fontSize: 18, fontWeight: 600 },
+
+    previewLanguage:       { left: 250, top: 760, width: 300, fontSize: 18, fontWeight: 600 },
+    previewHobbies:        { left: 250, top: 794, width: 300, fontSize: 18, fontWeight: 600 },
+    previewOther:          { left: 250, top: 829, width: 250, height: 80, fontSize: 18, fontWeight: 600, multiline: true },
+
+
+
+    /*==============================
+            CONTACT
+    ==============================*/
+
+    previewMobile:         { left: 700, top: 834, width: 250, fontSize: 18, fontWeight: 600 },
+
+
+
+    /*==============================
+        EDUCATION & SKILLS
+    ==============================*/
+
+    previewHighestQualification:{ left: 220, top: 989, width: 270, fontSize: 18, fontWeight: 600 },
+    previewCollege:             { left: 220, top: 1019, width: 270, fontSize: 18, fontWeight: 600 },
+    previewBoard12th:           { left: 220, top: 1049, width: 270, fontSize: 18, fontWeight: 600 },
+    previewBoard10th:           { left: 220, top: 1080, width: 270, fontSize: 18, fontWeight: 600 },
+    previewSpecialSkill:        { left: 220, top: 1109, width: 270, fontSize: 18, fontWeight: 600 },
+    previewEducationOther:      { left: 220, top: 1139, width: 270, fontSize: 18, fontWeight: 600 },
+
+
+
+
+
+    /*==============================
+            FAMILY
+    ==============================*/
+
+    previewFatherName:          { left: 690, top: 991, width: 250, fontSize: 18, fontWeight: 600 },
+    previewFatherOccupation:    { left: 690, top: 1021, width: 250, fontSize: 18, fontWeight: 600 },
+    previewMotherName:          { left: 690, top: 1052, width: 250, fontSize: 18, fontWeight: 600 },
+    previewMotherOccupation:    { left: 690, top: 1082, width: 250, fontSize: 18, fontWeight: 600 },
+    previewSiblingsDetails:     { left: 690, top: 1113, width: 250, height: 60, fontSize: 18, fontWeight: 600, multiline: true },
+
+
+
+    /*==============================
+          WORK & CAREER
+    ==============================*/
+
+    previewProfession:          { left: 220, top: 1244, width: 250, fontSize: 18, fontWeight: 600 },
+    previewOrganization:        { left: 220, top: 1272, width: 250, fontSize: 18, fontWeight: 600 },
+    previewWorkPlace:           { left: 220, top: 1301, width: 250, fontSize: 18, fontWeight: 600 },
+    previewIncome:              { left: 220, top: 1330, width: 250, fontSize: 18, fontWeight: 600 },
+
+
+    /*==============================
+            PARTNER
+    ==============================*/
+
+    previewPartnerQualification:{ left: 690, top: 1244, width: 300, fontSize: 18, fontWeight: 600 },
+    previewPartnerProfession:   { left: 690, top: 1272, width: 300, fontSize: 18, fontWeight: 600 },
+    previewPartnerLocation:     { left: 690, top: 1301, width: 300, fontSize: 18, fontWeight: 600 },
+    previewPartnerOther:        { left: 690, top: 1330, width: 300, fontSize: 18, fontWeight: 600 },
+
+
+
+    /*==============================
+            ADDRESS
+    ==============================*/
+
+    previewCurrentAddress:      { left: 175, top: 1430, width: 350, height: 80, fontSize: 18, fontWeight: 600, multiline: true },
+    previewPermanentAddress:    { left: 540, top: 1430, width: 350, height: 80, fontSize: 18, fontWeight: 600, multiline: true },
+
+    /*==============================
+            Sender
+    ==============================*/
+    previewSenderName:          { left:170, top:1508, width:250, fontSize:18, fontWeight:600 },
+    previewSenderMobile:        { left:705, top:1508, width:180, fontSize:18, fontWeight:600 },
+
+
+    /*==============================
+            PHOTO
+    ==============================*/
+
+    previewPhoto:               { left: 510, top: 185, width: 450, height: 570 }
+
+};
+
+
