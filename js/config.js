684bcbe291ec231cbd454db0f891ccd781ad0f2c
milansetuindia <milansetuindia@gmail.com>
2026-08-11 15:02:00 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@
 window.APP_NAME = "Jaiswal Vaivaahiki";
 
 window.API_URL =
-window.location.hostname === "localhost"
-  ? "http://localhost:5000"
-  : "https://backend-milan-setu.onrender.com";
+    window.location.hostname === "localhost"
+        ? "http://localhost:5000"
+        : "https://backend-jaiswal-vaivaahiki.onrender.com";
 
 window.RAZORPAY_KEY = "rzp_live_TMkrTHVVB3QcaY";
